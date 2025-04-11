857aa8d9e9bd9d20d6b995445dd743a4897ef8c4
intelligentRaji <sergdim7@gmail.com>
2025-04-12 01:22:22 +0200

    refactor: remove useless check inside inject function

--- src/di/utils/inject.ts
+++ src/di/utils/inject.ts
@@ -4,9 +4,5 @@ import { getCurrentInjector } from './context'
 export function inject<T extends Constructor>(target: T): InstanceType<T> {
   const injector = getCurrentInjector()
 
-  if (!injector) {
-    throw new Error('Injector is not initialized')
-  }
-
   return injector.get(target)
 }
