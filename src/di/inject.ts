46dce43f9974ae4246d60d3160e41bda40ed3fa1
intelligentRaji <sergdim7@gmail.com>
2025-08-09 10:34:25 +0200

    fix: fix inject() optional typing

--- src/di/inject.ts
+++ src/di/inject.ts
@@ -6,12 +6,12 @@ import { InjectionOptionalOptions, InjectionOptions } from './injector/injector'
 export function inject<T>(
   traget: InjectionToken<T> | Constructor<T>,
   options: InjectionOptionalOptions,
-): T | undefined
+): T | null
 export function inject<T>(traget: InjectionToken<T> | Constructor<T>, options?: InjectionOptions): T
 export function inject<T>(
   target: InjectionToken<T> | Constructor<T>,
   options: InjectionOptions | InjectionOptionalOptions = {},
-): T {
+): T | null {
   const injector = getCurrentInjector()
 
   return injector.get(target, options)
